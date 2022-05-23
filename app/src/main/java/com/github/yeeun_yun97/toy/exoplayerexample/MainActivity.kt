6c57b5c2b1aeb9235df5f3f30b12bfbe4856a865
yyn9704@gmail.com <yyn9704@gmail.com>
2022-05-23 16:49:24 +0900

    view binding

--- app/src/main/java/com/github/yeeun_yun97/toy/exoplayerexample/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/exoplayerexample/MainActivity.kt
@@ -2,10 +2,21 @@ package com.github.yeeun_yun97.toy.exoplayerexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.github.yeeun_yun97.toy.exoplayerexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
     }
+
+
+
+
+
+
+
 }
