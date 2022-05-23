ec2dd95182a904d4e75c2b6dbfc098fdbe9f3762
yyn9704@gmail.com <yyn9704@gmail.com>
2022-05-24 04:54:41 +0900

    media items

--- app/src/main/java/com/github/yeeun_yun97/toy/exoplayerexample/MainActivity.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/exoplayerexample/MainActivity.kt
@@ -11,13 +11,24 @@ import com.github.yeeun_yun97.toy.exoplayerexample.databinding.ActivityMainBindi
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
-import com.google.android.exoplayer2.PlayerMessage
-import com.google.android.exoplayer2.trackselection.MappingTrackSelector
+import com.google.android.exoplayer2.source.ShuffleOrder
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private var _player: ExoPlayer? = null
     private val player: ExoPlayer get() = _player!!
+
+    private val exampleUrl1 =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+    private val exampleUrl2 =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+    private val exampleUrl3 =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4"
+    private val exampleUrl4 =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerEscapes.mp4"
+    private val exampleUrl5 =
+        "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerFun.mp4"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,15 +53,71 @@ class MainActivity : AppCompatActivity() {
         // fire event at specified playback position
         player.createMessage { messageType, payload ->
             // Do something at the specified playback position.
-            Toast.makeText(this@MainActivity, "event 120 $messageType : ${payload.toString()}", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                this@MainActivity,
+                "event 120 $messageType : ${payload.toString()}",
+                Toast.LENGTH_LONG
+            ).show()
         }.setLooper(Looper.getMainLooper())
             .setPosition(0, 120_000)
             .setDeleteAfterDelivery(false)
             .send()
 
         // set playlist and prepare
-        val exampleVideoUrl = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
-        setPlaylistAndPrepare(listOf(exampleVideoUrl))
+        setPlaylistAndPrepare(listOf(exampleUrl1, exampleUrl2))
+
+        // modify playlist
+        modifyPlaylist()
+
+        //clear playlist
+        player.clearMediaItems()
+
+        // set entire playlist
+        setPlaylistByList()
+
+        /*
+        // playlist count
+        player.mediaItemCount
+
+        // get media item by index
+        player.getMediaItemAt(0)
+
+        //current playing item index
+        player.currentMediaItemIndex
+
+        // check next, get next
+        player.hasNextMediaItem()
+        player.nextMediaItemIndex
+        */
+
+
+        // repeat mode
+        setRepeatMode()
+
+        // shuffle mode
+        setShuffleMode()
+
+    }
+
+    private fun modifyPlaylist() {
+        player.moveMediaItem(0, 1)
+        player.removeMediaItem(0)
+        player.addMediaItem(MediaItem.fromUri(exampleUrl3))
+    }
+
+    private fun setPlaylistByList() {
+        val items: List<MediaItem> = listOf(
+            buildMediaItem(exampleUrl2, "ex2"),
+            MediaItem.fromUri(exampleUrl5),
+            MediaItem.fromUri(exampleUrl3),
+            MediaItem.fromUri(exampleUrl1),
+            MediaItem.fromUri(exampleUrl4),
+        )
+        player.setMediaItems(items, true)
+    }
+
+    private fun buildMediaItem(url: String, mediaId: String = "", tag: String = ""): MediaItem {
+        return MediaItem.Builder().setUri(url).setMediaId(mediaId).setTag(tag).build()
     }
 
     override fun onResume() {
@@ -67,15 +134,34 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setPlaylistAndPrepare(urls: List<String>) {
-        for(url in urls) {
+        for (url in urls) {
             val mediaItem = MediaItem.fromUri(url)
-            player.setMediaItem(mediaItem)
+            player.addMediaItem(mediaItem)
         }
         player.prepare()
     }
 
     private fun playPlayList() {
         player.play()
+    }
+
+    private fun setRepeatMode() {
+        player.repeatMode = Player.REPEAT_MODE_OFF //play next and don't repeat
+        Player.REPEAT_MODE_ONE // play one and repeat
+        Player.REPEAT_MODE_ALL // repeat playlist
+    }
+
+    private fun setShuffleMode() {
+        // if turn on shuffleMode, seekToNextMediaItem will play randomized next item
+        player.shuffleModeEnabled = true
+
+        val array : IntArray = listOf(3, 1, 0, 4, 2).toIntArray()
+
+        // set custom shuffle order
+        player.setShuffleOrder(
+            ShuffleOrder.DefaultShuffleOrder(array, 0L)
+        )
+
     }
 
 
--- app/src/main/java/com/github/yeeun_yun97/toy/exoplayerexample/PlayerListenerExample.kt
+++ app/src/main/java/com/github/yeeun_yun97/toy/exoplayerexample/PlayerListenerExample.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.PlaybackException
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.Timeline
 import com.google.android.exoplayer2.upstream.HttpDataSource
 
 class PlayerListenerExample : Player.Listener {
@@ -22,16 +23,16 @@ class PlayerListenerExample : Player.Listener {
     // playback error
     override fun onPlayerError(error: PlaybackException) {
         val cause = error.cause
-        if (cause is HttpDataSource.HttpDataSourceException){
+        if (cause is HttpDataSource.HttpDataSourceException) {
             //request data
             val requestDataSpec = cause.dataSpec
             Log.d("error on request data ", requestDataSpec.toString())
-            if(cause is HttpDataSource.InvalidResponseCodeException){
+            if (cause is HttpDataSource.InvalidResponseCodeException) {
                 val responseData = cause.responseBody
                 val responseCode = cause.responseCode
                 Log.d("error code and response", "$responseCode :  $responseData")
-            }else{
-               val causeOf =  cause.cause
+            } else {
+                val causeOf = cause.cause
                 Log.d("error cause", causeOf?.toString() ?: "null")
             }
         }
@@ -40,14 +41,14 @@ class PlayerListenerExample : Player.Listener {
     // playlist transitions
     override fun onMediaItemTransition(mediaItem: MediaItem?, reason: Int) {
         val tag = "playback state changed"
-        when(reason){
-            Player.MEDIA_ITEM_TRANSITION_REASON_REPEAT->
+        when (reason) {
+            Player.MEDIA_ITEM_TRANSITION_REASON_REPEAT ->
                 Log.d(tag, "repeat same item")
-            Player.MEDIA_ITEM_TRANSITION_REASON_AUTO->
+            Player.MEDIA_ITEM_TRANSITION_REASON_AUTO ->
                 Log.d(tag, "auto transition")
-            Player.MEDIA_ITEM_TRANSITION_REASON_SEEK->
+            Player.MEDIA_ITEM_TRANSITION_REASON_SEEK ->
                 Log.d(tag, "seek like player.next()")
-            Player.MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED->
+            Player.MEDIA_ITEM_TRANSITION_REASON_PLAYLIST_CHANGED ->
                 Log.d(tag, "playlist changed")
         }
     }
@@ -58,7 +59,7 @@ class PlayerListenerExample : Player.Listener {
         reason: Int
     ) {
         super.onPositionDiscontinuity(oldPosition, newPosition, reason)
-        when(reason){
+        when (reason) {
             Player.DISCONTINUITY_REASON_SEEK ->
                 // when seek called this way
                 true
@@ -80,8 +81,13 @@ class PlayerListenerExample : Player.Listener {
         // use onPlayWhenReadyChanged, onMediaItemTransition, etc
     }
 
-
-
-
+    // when media item added, removed moved
+    override fun onTimelineChanged(timeline: Timeline, reason: Int) {
+        when (reason) {
+            Player.TIMELINE_CHANGE_REASON_PLAYLIST_CHANGED ->
+                // update ui here to when modified playlist
+                Log.d("playlist have changed", "-")
+        }
+    }
 
 }
